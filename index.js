3b23a204f1edf99e2974b7fa3f848c4f74a373ed
Ubuntu <ubuntu@ip-172-31-7-43.us-west-2.compute.internal>
2022-06-24 13:47:23 +0000

    all files

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+    // Respond with "hello world" for requests that hit our root "/"
+    app.get("/", function (req, res) {
+     return res.send("Hello World");
+    });
+
+    // listen to port 3000 by default
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Server is running");
+    });
+
+    module.exports = app;
