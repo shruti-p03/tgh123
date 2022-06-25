5ed498a012ded27b1d8a4584a60dd34aa3f7ac6c
shruti-p03 <86187992+shruti-p03@users.noreply.github.com>
2022-06-25 23:02:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
     // Respond with "hello world" for requests that hit our root "/"
     app.get("/", function (req, res) {
-     return res.send("Hello tgh");
+     return res.send("Hello World");
     });
 
     // listen to port 3000 by default
